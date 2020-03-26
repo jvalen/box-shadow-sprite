0801c0ccc0ce52a5a6200cc320230b895eb724f4
jvalen <javiervalenciaromero@gmail.com>
2020-03-26 17:53:58 +0100

    Add dev-server for hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,15 @@ const path = require("path");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+  devtool: "inline-source-map",
   module: {
     rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
   },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
+  },
+  devServer: {
+    contentBase: "./dist"
   }
 };
